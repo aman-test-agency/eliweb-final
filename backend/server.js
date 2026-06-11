2140db53c585beb0ec4b6559c70e689e3ed43711
adarsh <amantest0106@gmail.com>
2026-06-12 01:06:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://www.eliweb.com",
+  "https://eliweb-final.vercel.app/",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
